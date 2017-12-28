3c5432c49bb6a5d6f13ff79435840b1389383d93
Matthew Johnson <matthew.johnson@intel.com>
2017-12-28 12:25:32 -0800

    Incorporate suggested changes
    Make suggested changes from review of PR #9 in matthewrsj/mixer-tools
    
    Signed-off-by: Matthew Johnson <matthew.johnson@intel.com>

--- swupd/manifest_test.go
+++ swupd/manifest_test.go
@@ -622,12 +622,12 @@ func TestHasTypeChanges(t *testing.T) {
 		},
 	}
 
-	if mUnchanged.hasTypeChanges() {
+	if mUnchanged.hasUnsupportedTypeChanges() {
 		t.Error("Manifest with no type changes detected to have type changes")
 	}
 
 	for _, m := range msChanged {
-		if !m.hasTypeChanges() {
+		if !m.hasUnsupportedTypeChanges() {
 			t.Error("Manifest with type changes detected to have no type changes")
 		}
 	}
